a52cfdebef07f4300d9ab2401e30df4d2e8fdcef
Dietmar Rieder <dietmar.rieder@i-med.ac.at>
2020-05-11 22:45:22 +0200

    - fix R script

--- bin/CCF.R
+++ bin/CCF.R
@@ -3,18 +3,22 @@ args = commandArgs(trailingOnly=TRUE)
 varDataIn <- args[1]
 outFile <- args[2]
 
-varData<-read.csv(varDataIn, sep="\t", stringsAsFactors=TRUE)
+varData<-read.table(varDataIn, sep="\t", header=TRUE)
 
+# get chr prefix
+chr_prefix <- grepl("chr", varData$Chr, fixed = TRUE)
 
 getCCF<-function(data) {
-  
+  # remove chr prefix, will be added again at the end
+  data$Chr <- gsub("chr","", data$Chr)
+
   # Restrict to Chromosomes 1-22
   data<-data[which(as.character(data$Chr) %in% 
-      as.character(seq(1:22))),]
-  
+      as.character(c(seq(1:22), "X", "Y"))),]
+
   # Select wanted columns
-  wantcol<-c("SubjectID",
-    "SampleType", 
+  wantcol<-c("PatientID",
+    "SampleType",
     "Chr",
     "pos",
     "normCnt",
@@ -25,9 +29,10 @@ getCCF<-function(data) {
     "cnB",
     "purity")
   ind<-match(wantcol, colnames(data))
+
   if (sum(is.na(ind)>0)) stop("Wrong data format!\n")
   indata<-data[,ind]
-  
+
   # Append new results
   outdata<-matrix(NA, nrow=nrow(indata), ncol=5)
   colnames(outdata)<-c("CCF",
@@ -35,20 +40,20 @@ getCCF<-function(data) {
     "CCF.95",
     "pSubclonal",
     "pClonal")
-  
+
   for (i in 1:nrow(indata)) {
 
     outdata[i,]<-absCCF2(indata[i,])
-    
+
   }
-  
+
   outdata<-cbind(indata, outdata)
   return(outdata)
-  
+
 }
 
 sub.cint<-function(x, n.alt, depth, prob=0.95) {
-  
+
   xnorm<-x/sum(x)
   xsort<-sort(xnorm, decreasing=TRUE)
   xcumLik<- cumsum(xsort)
@@ -60,7 +65,7 @@ sub.cint<-function(x, n.alt, depth, prob=0.95) {
   l.t<-cellu[1]
   r.t<-cellu[length(cellu)]
   m<-cellu[which.max(cint)]
-  
+
   prob.subclonal<-sum(xnorm[1:90])
   prob.clonal<-sum(xnorm[91:100])
 
@@ -69,38 +74,40 @@ sub.cint<-function(x, n.alt, depth, prob=0.95) {
     abs.CCF.95=r.t,
     pSub=prob.subclonal,
     pClo=prob.clonal)
-  
+
   return(out)
-  
+
 }
 
 f.function<-function (c, purity, local.copy.number) {
-  
   res<-min(c((purity*c)/(2*(1-purity)+purity*local.copy.number),1))
+
   return(res)
-  
 }
 
 absCCF2<-function(idata) {
-  
   n.alt<-idata[,"mutCnt"]
   depth<-idata[,"mutCnt"]+idata[,"normCnt"]
   purity<-idata[,"purity"]
-  local.copy.number<-idata[,"cnA"]
-  
+  local.copy.number<-idata[,"ploidy"]
+
+
   x<-dbinom(n.alt, depth, 
     prob=sapply(seq(0.01,1,length.out=100),
       f.function,purity,local.copy.number))
-  
+
   if(min(x)==0) x[length(x)]<-1
-  
+
   names(x)<-seq(0.01,1,length.out=100)
   est<-sub.cint(x, n.alt, depth)
-  
+
   return(est)
-  
+
 }
 
 CCFest<-getCCF(varData)
 
-write.table(CCFest, file=outFile, sep="\t", quote=FALSE)
+# readd chr prefix
+CCFest$Chr[chr_prefix] <- paste0("chr", CCFest$Chr[chr_prefix])
+
+write.table(CCFest, file=outFile, sep="\t", quote=FALSE, row.names = FALSE)
